a164b26d4d44af5f81165f749fcb53f109f25bfd
Fred <96602008+FredericGariepy@users.noreply.github.com>
2024-05-18 02:14:27 -0400

    Update setup.py

--- Python 201/content/Extending/Py2exe/setup.py
+++ Python 201/content/Extending/Py2exe/setup.py
@@ -3,17 +3,14 @@ Convert Python scripts into standalone executable Windows programs (EXE files).
 Allows Python programs to be run on Windows systems without requiring a Python interpreter.
 
 '''
-
-from distutils.core import setup
-import py2exe
-
-setup(
-    options={
-        'py2exe': {
-            'bundle_files': 1,  # Bundle everything into a single file
-            'compressed': True,  # Compress the library archive
-        }
+from py2exe import freeze
+freeze(
+  console = [{'script':'hello.py'}], # Specify the script to convert
+  options = {
+      'py2exe': {
+          'bundle_files': 1,  # Bundle everything into a single file
+          'compressed' : True # Compress the library archive
+      }
     },
-    windows=[{'script': 'hello.py'}], # Specify the script to convert
-    zipfile=None,  # No separate zip file
+    zipfile = None # No separate zip file
 )
